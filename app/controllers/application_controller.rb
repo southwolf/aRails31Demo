06c4b31668ffd543bba78355f18afd0ed44fc5a7
SouthWolf <wp.southwolf@gmail.com>
2011-11-22 13:18:03 +0800

    Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :admin?
+  before_filter :authorize
+  
   protect_from_forgery
+  
+  protected
+  
+  def admin?
+    session[:name] == "login" && session[:pw] == "password"
+  end
+  
+  def authorize
+    unless admin?
+      redirect_to root_url
+    end    
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  skip_filter :authorize
+  
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    session[:pw] = params[:password]
+    if admin?
+      redirect_to root_url, :notice => "Successfully logged in" 
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = "Logged out"
+    redirect_to root_url
+  end
+end
